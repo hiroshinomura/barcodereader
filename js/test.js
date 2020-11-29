0fdec8cfcb10a35c5e1ea8bb799097195112a057
hiroshinomura <hiro.nomu@gmail.com>
2020-11-29 14:21:41 +0900

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,105 @@
+function sound()
+{
+	// 対象となるID名
+	var id = 'sound-file' ;
+
+	// 初回以外だったら音声ファイルを巻き戻す
+	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( id ).currentTime = 0;
+	}
+
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( id ).play() ;
+}
+
+$(function () {
+
+    startScanner();
+
+});
+
+const startScanner = () => {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#photo-area'),
+            constraints: {
+                decodeBarCodeRate: 3,
+                successTimeout: 500,
+                codeRepetition: true,
+                tryVertical: true,
+                frameRate: 15,
+                width: 640,
+                height: 480,
+                facingMode: "environment"
+            },
+        },
+        decoder: {
+            readers: [
+                "i2of5_reader"
+            ]
+        },
+
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+    });
+
+    Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {
+                        x: 0,
+                        y: 1
+                    }, drawingCtx, {
+                        color: "green",
+                        lineWidth: 2
+                    });
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {
+                    x: 0,
+                    y: 1
+                }, drawingCtx, {
+                    color: "#00F",
+                    lineWidth: 2
+                });
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {
+                    x: 'x',
+                    y: 'y'
+                }, drawingCtx, {
+                    color: 'red',
+                    lineWidth: 3
+                });
+            }
+        }
+    });
+
+    //barcode read call back
+    Quagga.onDetected(function (result) {
+        document.getElementById( 'sound-file' ).play() ;
+        console.log(result.codeResult.code);
+    });
+}
